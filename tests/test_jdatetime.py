5ca3e1110e3559ed018334acddf9a232b87400bb
Vincent Gao <gaobing1230@gmail.com>
2026-07-15 09:59:13 +0200

    Make hour12 property private (_hour12)

--- tests/test_jdatetime.py
+++ tests/test_jdatetime.py
@@ -221,7 +221,7 @@ class TestJDateTime(TestCase):
                 std = _std.datetime(2020, 1, 1, hour, 5, 0)
                 self.assertEqual(jd.strftime('%I'), std.strftime('%I'))
                 self.assertEqual(jd.strftime('%-I'), std.strftime('%-I'))
-                self.assertEqual(jd.hour12, int(std.strftime('%I')))
+                self.assertEqual(jd._hour12, int(std.strftime('%I')))
                 # %I must be consistent with the (correct) %p directive:
                 # reconstructing the 24-hour value from (%p, %I) round-trips.
                 twelve = int(jd.strftime('%I'))
